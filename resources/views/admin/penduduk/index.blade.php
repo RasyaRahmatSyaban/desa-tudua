4c57e3a8ca194d99d783ab09e60a85904612a2b3
RasyaRahmatSyaban <rasyarahmatsyban10@gmail.com>
2025-07-29 00:18:55 +0800

    minor changes

--- resources/views/admin/penduduk/index.blade.php
+++ resources/views/admin/penduduk/index.blade.php
@@ -131,7 +131,7 @@
                                 {{ $item->alamat }}
                             </td>
                             <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                {{ $item->kepalaKeluarga->nama ?? '-' }}
+                                {{ $item->kepalaKeluarga->nama ?? 'Ya' }}
                             </td>
                             <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
                                 <div class="flex items-center space-x-2">
