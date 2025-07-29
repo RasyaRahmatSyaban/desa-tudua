2dd29756c83e538a1cd582f8cae0de36a9867869
RasyaRahmatSyaban <rasyarahmatsyban10@gmail.com>
2025-07-29 16:45:04 +0800

    refactor(perangkat-desa) : make nip unique

--- database/migrations/2025_06_16_064527_create_perangkat_desas_table.php
+++ database/migrations/2025_06_16_064527_create_perangkat_desas_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('perangkat_desa', function (Blueprint $table) {
             $table->id();
             $table->string('nama', 100);
-            $table->string('nip', 30);
+            $table->string('nip', 30)->unique();
             $table->string('jabatan', 100);
             $table->string('foto')->nullable();
             $table->timestamps();
